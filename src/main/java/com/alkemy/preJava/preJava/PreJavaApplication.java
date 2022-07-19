e746ad4780c63d7cc5445c51c671046304a6f51c
Andrea Assale <andre.assale87@gmail.com>
2022-07-19 16:40:18 -0300

    Initial commit

---
+++ src/main/java/com/alkemy/preJava/preJava/PreJavaApplication.java
@@ -0,0 +1,14 @@
+package com.alkemy.preJava.preJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreJavaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/alkemy/preJava/preJava/PreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.preJava.preJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
